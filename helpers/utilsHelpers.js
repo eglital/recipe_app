6cca8820dba8cd94916f111183290597e1aa5575
Egle Libby <eglelibby@gmail.com>
2017-05-05 01:28:26 -0400

    add buttons to edit or delete your recipes

--- helpers/utilsHelpers.js
+++ helpers/utilsHelpers.js
@@ -6,4 +6,9 @@ UtilsHelper.formatDate = function(date) {
   return moment(date).format("MMM Do YY h:mmA");
 };
 
+UtilsHelper.isOwner = function(id1, id2) {
+  console.log(id1, id2);
+  return id1.toString() === id2.toString();
+};
+
 module.exports = UtilsHelper;
